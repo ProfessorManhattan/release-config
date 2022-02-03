b40f396cb778d78fc0ce12ef13a658429bda7f97
Brian Zalewski <brian@megabyte.space>
2022-02-02 21:04:22 -0500

    🐛 fix(testing): testing Semantic

--- src/main.ts
+++ src/main.ts
@@ -102,7 +102,8 @@ const plugins: any = [
       changelogTitle:
         '# Changelog\n\nAll notable changes to this project will be documented in this file. In order to maintain \
         this file through automation, all commits to this repository must adhere to the guidelines laid out by \
-        [Conventional Commits](https://conventionalcommits.org).'
+        [Conventional Commits](https://conventionalcommits.org) and the \
+        [Megabyte Labs Commit Guide](https://megabyte.space/docs/contributing/commits).'
     }
   ],
   [
